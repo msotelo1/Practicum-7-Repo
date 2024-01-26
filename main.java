a62f8562ba869f6c4b4ec20c0a44ac4500f24c75
Chuck476 <cdierbach@towson.edu>
2024-01-26 17:52:42 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,5 @@
+// Practicum 7 - Fahrenheit to Celsius Program
 
+// ----------------------------------------------------------
+// This program will convert Fahrenheit to Celsius
+// ----------------------------------------------------------
