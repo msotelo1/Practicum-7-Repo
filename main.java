9be89d466e1feac14ea7babbd959d1d264e229fa
msotelo1 <158190815+msotelo1@users.noreply.github.com>
2024-02-06 03:22:03 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,21 @@
-// Practicum 7 - Fahrenheit to Celsius Program
+import java.util.*;
 
-// ----------------------------------------------------------
-// This program will convert Fahrenheit to Celsius
-// ----------------------------------------------------------
+public class Main {
+  public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("\nConverts temperatures in Fahrenheit to Celsius\n");
+
+    System.out.println("Enter temperature (in Fahrenheit): ");
+    double fah_temp = input.nextDouble();
+
+   System.out.printf("%.0f degrees Fahrenheit = %.1f degrees Celsius", fah_temp, convertToCelsius(fah_temp));
+  
+  }
+
+  public static double convertToCelsius(double f) {
+    return (f-32) * 5/9;
+  }
+  
+}
